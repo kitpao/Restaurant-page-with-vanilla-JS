01c932327c9208f922ff61a87dd449f8d93c1630
kitpao <kitpao91@gmail.com>
2020-11-19 16:52:23 -0600

    :construction: Add and link file for the main components

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 import styles from './styles/homepage'
+import {function1, function2} from './mainContent';
+
+function initialComponents() {
+  // select the main div
+  // create the left div element with contents
+  console.log(function1());
+  console.log(function2());
+}
+
+// document.body.appendChild(initialComponents()); append new div element
+initialComponents();
---
+++ src/mainContent.js
@@ -0,0 +1,8 @@
+const function1 = () => 'function1 printed';
+
+const function2 = () => 'function2 printed';
+
+export {
+  function1,
+  function2,
+}
